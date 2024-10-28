a31832008d23d5ebb3bc2a26cca65786c5e3373f
Javier Monasterio - PC Cibernos <jmonasterio@cibernos.com>
2024-10-28 18:03:49 +0100

    # Cargar el archivo .env load_dotenv()

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+from dotenv import load_dotenv
+import os
+
+# Cargar el archivo .env
+load_dotenv()
 
 #DATABASE_URL = "postgresql://myuser:password@localhost:5433/fastapi_database"
-DATABASE_URL = "postgresql://sgmaquser01:K0KDawfcAEKs5ze3eImeLHU6nEOXUXqP@dpg-csdujgbtq21c73a93oa0-a.frankfurt-postgres.render.com:5432/fastapidb_rspb"
+#DATABASE_URL = "postgresql://sgmaquser01:K0KDawfcAEKs5ze3eImeLHU6nEOXUXqP@dpg-csdujgbtq21c73a93oa0-a.frankfurt-postgres.render.com:5432/fastapidb_rspb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = _sql.create_engine(DATABASE_URL)
 
