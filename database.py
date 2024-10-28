96022146c661cafe1e150a316b37004dbd77e95a
Javier Monasterio - PC Cibernos <jmonasterio@cibernos.com>
2024-10-28 17:52:05 +0100

    DATABASE_URL

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://myuser:password@localhost:5433/fastapi_database"
+#DATABASE_URL = "postgresql://myuser:password@localhost:5433/fastapi_database"
+DATABASE_URL = "postgresql://sgmaquser01:K0KDawfcAEKs5ze3eImeLHU6nEOXUXqP@dpg-csdujgbtq21c73a93oa0-a.frankfurt-postgres.render.com:5432/fastapidb_rspb"
 
 engine = _sql.create_engine(DATABASE_URL)
 
